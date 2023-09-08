c22c97f2e45bedf83f54867a9f5e49a811e7dd1d
Jesus Leon <ing.jesusleon@gmail.com>
2023-09-08 18:46:03 -0400

    Astro Api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://astro.build',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
 });
